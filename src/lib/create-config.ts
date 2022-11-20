4ed374d90710e9c5745128225287712a969e5adc
Mirror <asok-mirror@users.noreply.github.com>
2022-11-20 18:56:21 -0500

    added configurable parameters

--- src/lib/create-config.ts
+++ src/lib/create-config.ts
@@ -118,7 +118,7 @@ const dateHandlingCallback = (connection, callback) => {
 const defaultDbOptions: IDBOption = {
     user: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    host: process.env.DATABASE_HOST,
+    host: process.env.DATABASE_HOST || 'localhost',
     port: parseEnvVarNumber(process.env.DATABASE_PORT, 5432),
     database: process.env.DATABASE_NAME || 'unleash',
     ssl:
